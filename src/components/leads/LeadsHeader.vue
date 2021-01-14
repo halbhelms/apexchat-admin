7398b88e91eebc80967fbbb1715512939d2acf16
Hal Helms <hal.helms@gmail.com>
2021-01-14 10:46:51 -0800

    vac guy

--- src/components/leads/LeadsHeader.vue
+++ src/components/leads/LeadsHeader.vue
@@ -1,6 +1,27 @@
 <template>
-    <div class="header">
+    <!-- <div class="header">
         <div class="title">Leads <span class="title-company"> {{ companyName }}</span></div>
+    </div> -->
+    <div class="dashboard-header-container">
+        <div class="header">
+            <!-- title -->
+            <div class="title">Leads <span class="title-company"> {{ companyName }}</span></div>
+            <!-- time filter -->
+            <div class="date-filter">
+                <div class="filter">
+                    <input name="date-filter" type="radio" id="last-login" @change="setDateFilter('lastLogin')" />
+                    <label for="last-login" name="date-filter"> Since last login</label>
+                </div>
+                <div class="filter">
+                    <input name="date-filter" type="radio" id="last-30" @change="setDateFilter('last30')" />
+                    <label for="last-30" name="date-filter"> Last 30 Days</label>
+                </div>
+                <div class="filter">
+                    <input name="date-filter" type="radio" id="last-60" @change="setDateFilter('last60')" />
+                    <label for="last-60" name="date-filter"> Last 60 days</label>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -20,7 +41,7 @@
         computed: {
             companyName() {
                 if (this.$route.params.id) {
-                    return 'for' + this.$store.getters.getCompanyById(this.$route.params.id).name} else {
+                    return 'for ' + this.$store.getters.getCompanyById(this.$route.params.id).name} else {
                         return ''
                     }
             }
@@ -29,10 +50,21 @@
 </script>
 
 <style scoped>
+    .date-filter {
+           display: grid;
+        grid-template-columns: 50% auto;
+        padding-top: 8px;
+        font-size: 0.65rem;
+    }
 
     .header {
-        box-shadow: 0 0 6px 0 silver;
-        background-color: rgb(237,240,245);
+        /* box-shadow: 0 0 6px 0 silver;
+        background-color: rgb(237,240,245); */
+        /* width: 780px; */
+        display: grid;
+        grid-template-columns: 472px 308px;
+        text-align: left;
+        border: 1px solid silver;
     }
 
     .title {
