8714656fe92a5e60165f65d75bdac185b9dc60c5
Hal Helms <hal.helms@gmail.com>
2021-02-18 15:48:50 -0800

    Cleanup

--- src/components/companies/CompanyInfoForm.vue
+++ src/components/companies/CompanyInfoForm.vue
@@ -1,25 +1,24 @@
 <template>
     <div v-if="inDev" class="inDev">{{ $options.name }}</div>
     <section-header>Company Info</section-header>
-    <div class="company-info-form">
-        <base-input _label="Company name" _id="company-name" v-model="company.name"></base-input>
-        <base-input _label="Website address" _id="website-address" v-model="company.domain"></base-input>
-        <base-input _label="Logo url" _id="logo-url" v-model="company.logo_url"></base-input>
-        
-        <base-select _label="Time zone" :_styles="styles.timeZone" _id="time-zone" v-model="company.time_zone" :_options="timeZones"></base-select>
-        
-        <base-input _label="Owner" _id="owner" v-model="company.owner"></base-input>
-        <base-input _label="Customer since (year)" _id="since" v-model="company.since"></base-input>
-        <base-input _label="Email" _type="email" _id="email" v-model="company.email"></base-input>
-        <base-input _label="Phone" _type="tel" _id="phone" v-model="company.phone"></base-input>
-        <base-input _label="Apex company ID" _id="apex-company-id" v-model="company.apex_company_id"></base-input>
-        <base-textarea _label="Apex info" _id="apex-info" v-model="company.apex_info"></base-textarea>
+    <form @submit.prevent="submitForm">
+        <div class="company-info-form">
+            <base-input _label="Company name" _id="company-name" v-model="company.name"></base-input>
+            <base-input _label="Website address" _id="website-address" v-model="company.domain"></base-input>
+            <base-input _label="Logo url" _id="logo-url" v-model="company.logo_url"></base-input>
+            <base-select _label="Time zone" :_styles="styles.timeZone" _id="time-zone" v-model="company.time_zone" :_options="timeZones"></base-select>
+            <base-input _label="Owner" _id="owner" v-model="company.owner"></base-input>
+            <base-input _label="Customer since (year)" _id="since" v-model="company.since"></base-input>
+            <base-input _label="Email" _type="email" _id="email" v-model="company.email"></base-input>
+            <base-input _label="Phone" _type="tel" _id="phone" v-model="company.phone"></base-input>
+            <base-input _label="Apex company ID" _id="apex-company-id" v-model="company.apex_company_id"></base-input>
+            <base-textarea _label="Apex info" _id="apex-info" v-model="company.apex_info"></base-textarea>
+            <base-checkbox _label="Self manage website?" :_styles="styles.selfManage" _id="self-managed-web" v-model="company.self_managed_web"></base-checkbox>
+            <base-select _label="Active status" _id="status" v-model="company.status" :_options="statusOptions" :_styles="styles.activeStatus" class="select"></base-select>
 
-        <base-checkbox _label="Self manage website?" :_styles="styles.selfManage" _id="self-managed-web" v-model="company.self_managed_web"></base-checkbox>
-
-        <base-select _label="Active status" _id="status" v-model="company.status" :_options="statusOptions" :_styles="styles.activeStatus" class="select"></base-select>
-        <base-button _width="w-medium" _radius="r-full" @button-clicked="submitForm">{{ buttonLabel }}</base-button>
-    </div>
+            <base-button :_styles="styles.submitButton"  @button-clicked="submitForm">{{ buttonLabel }}</base-button>
+        </div>
+    </form>
 </template>
 
 <script>
@@ -65,6 +64,15 @@
                         textAlign: 'left',
                         fontSize: '.8rem',
                         marginTop: '16px',
+                    },
+                    submitButton: {
+                        base: {
+
+                        },
+                        button: {
+                            borderRadius: '20px',
+                            width: '82px',
+                        }
                     }
                 }
             }
@@ -79,6 +87,10 @@
                 } else {
                     this.$store.dispatch('add_company', this.company)
                 }
+            },
+
+            validateForm() {
+                Object.keys
             }
         },
 
